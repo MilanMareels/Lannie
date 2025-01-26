dec7dc9fa4333c623e64f3fbcfbd6aa899294a3e
MilanMareels <milan.mareels@gmail.com>
2025-01-26 16:25:46 +0100

    headwr change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,8 @@
-import { Carousel, CustomFlowbiteTheme, FlowbiteCarouselTheme } from "flowbite-react";
+import {
+  Carousel,
+  CustomFlowbiteTheme,
+  FlowbiteCarouselTheme,
+} from "flowbite-react";
 import { headerImages } from "../data/headerImages";
 
 export default function Header() {
@@ -11,7 +15,7 @@ export default function Header() {
 
   return (
     <header>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-[600px]">
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-[600px] grayscale">
         <Carousel slideInterval={3000} theme={customTheme}>
           {headerImages.map((image, i) => (
             <img key={i} src={image.image} alt={`Header-foto-${i + 1}`} />
--- src/data/headerImages.ts
+++ src/data/headerImages.ts
@@ -1,1 +1,6 @@
-export const headerImages = [{ image: "./header-foto-2.jpg" }, { image: "./header-foto-1.jpg" }];
+export const headerImages = [
+  { image: "./header-foto-2.jpg" },
+  { image: "./header-foto-1.jpg" },
+  { image: "./basic-foto-1.jpg" },
+  { image: "./basic-foto-3.jpg" },
+];
