a586c52ea52ad6303aca257525a8f19b710f906a
MilanMareels <milan.mareels@gmail.com>
2025-02-12 20:05:22 +0100

    fix mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     >
       <NavBarSection />
       <section className="max-w-[1200px] w-full mx-auto px-4 text-white text-left mt-32">
-        <h1 className="text-6xl md:text-8xl w-[70%]">Ben jij al online? Of is je website aan vernieuwing toe</h1>
+        <h1 className="text-6xl md:text-8xl w-[70%]">Ben jij al online?</h1>
         <p className="text-lg mt-4">Dan is het tijd voor een professionele website!</p>
         <ContactButton />
       </section>
