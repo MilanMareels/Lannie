6d5f3fb262ffe8cb1541ec2712be0cf35646ecc7
MilanMareels <milan.mareels@gmail.com>
2025-03-26 18:24:19 +0100

    fix google ana, schemas nog doen!

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -5,10 +5,23 @@ import ServiceSection from "../components/ServiceSection";
 import CookieConsent from "../components/cookie/CookieConsent";
 import Acordion from "../components/ui/Acordion";
 import SEO from "../components/SEO/SEO";
+import ReactGA from "react-ga4";
 import { SEOContent } from "../data/SEO/SEO";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Page() {
+  const GI_ID: string = import.meta.env.VITE_GOOGLE_ANA!;
   const [cookies] = useCookies(["cookieContent"]);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (cookies.cookieContent) {
+      ReactGA.initialize(GI_ID);
+      ReactGA.send({ hitType: "pageview", page: `/${location.pathname}` });
+    }
+  }, [cookies.cookieContent, location]);
+
   return (
     <main>
       <SEO {...SEOContent.home} />
