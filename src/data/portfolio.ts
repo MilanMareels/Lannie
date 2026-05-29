f79e8c8c688bb413d112f7c5ef22e5f152716543
MilanMareels <milan.mareels@gmail.com>
2026-05-29 18:19:33 +0200

    added new items to portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -23,4 +23,16 @@ export const portfolio = [
     title: "Atelier Synaeve",
     link: "https://www.ateliersynaeve.be",
   },
+  {
+    img: "/wendy-de-nys.webp",
+    category: "Schoonheidsadvies",
+    title: "Schoonheidsadvies Wendy",
+    link: "https://wendydenys.be/",
+  },
+  {
+    img: "/hybrid-pt.webp",
+    category: "Sport & Fitness",
+    title: "Hybrid PT",
+    link: "https://www.hybridpt.be/",
+  },
 ];
